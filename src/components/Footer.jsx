91b7bf05be68a7fa2925fbabf848c5df8929c91f
arnobeek <arnoldbeeka@gmail.com>
2026-02-08 14:37:33 +0300

    Editted footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,12 +6,12 @@ import { IoMailOutline } from "react-icons/io5";
 export default function Footer() {
 
     return (
-        <div className='px-10 bg-[#3a5a40] pt-10'>
+        <div className='px-10 bg-[#3a5a40] pt-10 text-white'>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 py-5'>
                 <div className=' flex flex-col w-50 gap-3'>
                     <div className="flex items-center gap-2">
                         <img src={logo} className="w-15" />
-                        <h1 className="text-2xl font-extrabold">MODUS SAFARIS</h1>
+                        <h1 className="text-2xl font-extrabold ">MODUS SAFARIS</h1>
                     </div>
                     <p className="text-white">Curating extraordinary East African journeys for the discerning traveler.</p>
                     <div className='flex justify-between'>
@@ -35,7 +35,7 @@ export default function Footer() {
                     </div>
                 </div>
                 <div className='flex flex-col justify-between gap-3 text-sm'>
-                    <p className="text-black">DESTINATIONS</p>
+                    <p className="text-xl">DESTINATIONS</p>
                     <Link className="text-white">Uganda</Link>
                     <Link className="text-white">Kenya</Link>
                     <Link className="text-white">Tanzania</Link>
@@ -43,24 +43,24 @@ export default function Footer() {
                     
                 </div>
                 <div className='flex flex-col justify-between gap-3 text-sm'>
-                    <p className="text-black">COMPANY</p>
+                    <p className="text-xl">COMPANY</p>
                     <Link className="text-white">About Us</Link>
                     <Link className="text-white">Sustainability</Link>
                     <Link className="text-white">Travel Advisors</Link>
                     <Link className="text-white">Stories</Link>
                 </div>
                 <div className='flex flex-col justify-between gap-3 text-sm'>
-                    <p className="text-black">CONTACT</p>
+                    <p className="text-xl">CONTACT</p>
                     <div>
-                        <p className='text-black'>PHONE</p>
+                        <p className="text-xl">PHONE</p>
                         <p className="text-white"><a href="tel:+256756733094">+256-756-733-094</a></p>
                     </div>
                     <div>
-                        <p className='text-black'>EMAIL</p>
+                        <p className="text-xl">EMAIL</p>
                         <p className="text-white"><a href="mailto:info@modussafaris.com">info@modussafaris.com</a></p>
                     </div>
                     <div>
-                        <p className='text-black'>OFFICE</p>
+                        <p className="text-xl">OFFICE</p>
                         <p className="text-white">Mbarara, Uganda</p>
                     </div>
                 </div>
